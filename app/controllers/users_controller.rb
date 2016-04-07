a4a3c23674dccd4ca1a1c3eb12557eb89cdb1ae1
Vivek Patel <vnp229@gmail.com>
2016-04-07 08:34:56 -0500

    fixed bug with user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user, only: [:new]
+  skip_before_action :authenticate_user, only: [:new, :create]
   def new
     @user = User.new
   end
